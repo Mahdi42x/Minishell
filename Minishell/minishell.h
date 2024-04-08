8a10c80c5df33856925f67c44f28bff51e6d330e
Mahdi Awada <mawada@c4b10c4.42berlin.de>
2024-04-08 16:48:47 +0200

    first

---
+++ Minishell/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mawada <mawada@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/16 15:17:46 by mawada            #+#    #+#             */
+/*   Updated: 2024/04/08 16:33:48 by mawada           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL.H
+# define MINISHELL.H
+
+# include <sys/time.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <signal.h>
+# include "../libft/libft.h"
+
+
+#endif
